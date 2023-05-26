2bac09592373454ea0b0c0b1c64e0f2b7849d4a8
Denys Tereshchuk <terrya@ukr.net>
2023-05-26 14:20:54 +0400

    Updated views.py & removed unnecessary function

--- proxy/views.py
+++ proxy/views.py
@@ -1,63 +1,63 @@
+import re
 from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup
 from django.http import HttpResponse, HttpRequest
 
+from proxy.utils.change_elements import modify_soup_links
+
+
 BASE_URL = "https://news.ycombinator.com"
 
-
-def check_word(word: str) -> bool:
-    """Check that the word consists exactly of 6 letters"""
-    if len(word) == 6 and word[-1] not in (",", ".", "!", "?", ":", ";"):
-        return True
-    return False
-
-
-def add_spaces(element: BeautifulSoup, words: list[str]) -> None:
-    """Add space to the first or to the last word if element has space in the
-    start or in the end of element"""
-    if element[0] == " ":
-        words[0] = " " + words[0]
-    if element[-1] == " ":
-        words[-1] += " "
-
-
-def change_elements(
-    soup: BeautifulSoup, element_name: str, url: str, attr_name: str
-) -> None:
-    """Change url in attr for element in soup by element's name"""
-    for elem in soup.find_all(element_name, **{attr_name: True}):
-        elem[attr_name] = urljoin(url, elem[attr_name])
-
-
-def modify_element(element: BeautifulSoup) -> BeautifulSoup:
-    """Modify the element by adding "™" to words that have 6 letters"""
-    words = element.split()
-    if words:
-        modified_words = [
-            word + "™" if check_word(word) else word for word in words
-        ]
-        add_spaces(element, modified_words)
-        element.string.replace_with(" ".join(modified_words))
+EXT_CONTENT_TYPE = {
+    "css": "text/css",
+    "js": "application/javascript",
+    "svg": "image/svg+xml",
+    "ico": "image/x-icon",
+}
 
 
 def hacker_news_proxy(request: HttpRequest) -> HttpResponse:
-    url = urljoin(BASE_URL, request.get_full_path())
+    path = request.get_full_path()
+    host = (
+        request.build_absolute_uri("/")
+        if "SERVER_NAME" in request.META.keys()
+        else "http://127.0.0.1:8232"
+    )
+    url = urljoin(BASE_URL, path) if host not in path else path
 
     response = requests.get(url)
+
+    for ext in EXT_CONTENT_TYPE.keys():
+        if ext in path:
+            with open(path, "r") as f:
+                file = f.read()
+
+            return HttpResponse(file, content_type=EXT_CONTENT_TYPE[ext])
+
     soup = BeautifulSoup(response.content, "html.parser")
 
     for element in soup.find_all(text=True):
-        modify_element(element)
+        element.string.replace_with(
+            re.sub(r"\b\w{6}\b", lambda word: word.group() + "™", element)
+        )
 
     for element_name, attr_name in [
         ("img", "src"),
         ("script", "src"),
         ("link", "href"),
     ]:
-        change_elements(
-            soup=soup, element_name=element_name, attr_name=attr_name, url=url
+        modify_soup_links(
+            soup=soup,
+            element_name=element_name,
+            attr_name=attr_name,
+            url=url,
+            base_url=BASE_URL,
         )
 
-    return HttpResponse(soup)
+    for elem in soup.find_all("a", href=True):
+        if BASE_URL in elem["href"]:
+            elem["href"] = elem["href"].replace(BASE_URL, host)
+
+    return HttpResponse(str(soup))
